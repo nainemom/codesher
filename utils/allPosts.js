89f16f65f3445731800c9e01cf456c17cc4e5971
nainemom <nainemom@gmail.com>
2018-08-08 18:21:33 +0430

    add new key for posts for server-side calculated fields

--- utils/allPosts.js
+++ utils/allPosts.js
@@ -1,5 +1,7 @@
 var path = require('path')
 var fs = require('fs')
+var md5 = require('blueimp-md5')
+var IDate = require('idate')
 
 // all posts and contents
 const posts = (function () {
@@ -7,10 +9,24 @@ const posts = (function () {
   var posts = fs.readdirSync(dir)
   var ret = []
   for (var i = 0; i < posts.length; i++) {
-    ret.push({
+    const post = {
       name: posts[i].split('.')[0],
-      data: JSON.parse(fs.readFileSync(dir + '/' + posts[i]))
+      data: JSON.parse(fs.readFileSync(dir + '/' + posts[i])),
+      calculatedData: {}
+    }
+    post.calculatedData.authors = post.data.authors.map(email => {
+      const hash = md5(email)
+      const image = 'https://www.gravatar.com/avatar/' + hash + '.jpg'
+      return {
+        email,
+        image,
+        hash,
+        name: email.split('@')[0]
+      }
     })
+    const pdate = new IDate(post.data.date)
+    post.calculatedData.pdate = pdate.toString(true)
+    ret.push(post)
   }
   return ret
 })()
