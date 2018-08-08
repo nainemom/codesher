896f8ce0eaba629b7cefc14fcd498d8600e35f42
nainemom <nainemom@gmail.com>
2018-08-09 02:42:37 +0430

    add ofline-plugin

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 import main from './main.vue'
-import routes from './routes/dynamic.js'
+import routes from './routes/index.js'
 import './style.scss'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  mode: 'history',
   routes
 })
+
+OfflinePluginRuntime.install()
 
 document.addEventListener('DOMContentLoaded', function () {
   new Vue({
