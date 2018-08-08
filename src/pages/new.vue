e08a91d019f7d57708dec0912ae5d05d5de8fefc
nainemom <nainemom@gmail.com>
2018-08-08 11:47:33 +0430

    create new post page

---
+++ src/pages/new.vue
@@ -0,0 +1,16 @@
+<template>
+  <div> <h2>Post Creator App</h2></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {},
+  created() {
+    const prerender = require("../../utils/prerender.js");
+    if (prerender.hasAccess()) {
+      prerender.done();
+    }
+  }
+};
+</script>
