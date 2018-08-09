cf4ebd4c8102004ba368aff7b9ef4fe04a9f4b2f
nainemom <nainemom@gmail.com>
2018-08-10 02:01:02 +0430

    active sw only on production

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import AppLoading from './components/loading.vue'
 import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 import main from './main.vue'
 import routes from './routes/index.js'
 import './style.scss'
 
 Vue.use(VueRouter)
+Vue.prototype.$loading = Vue.loading = new (Vue.extend(AppLoading))()
 
 const router = new VueRouter({
   routes
 })
 
-OfflinePluginRuntime.install()
+if (process.env.NODE_ENV === 'production') {
+  OfflinePluginRuntime.install()
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   new Vue({
