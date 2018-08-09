80db967d4af2158c582ea7b687e7b11c27e8042d
nainemom <nainemom@gmail.com>
2018-08-10 02:56:55 +0430

    split chunks

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var WebpackPwaManifest = require('webpack-pwa-manifest')
 var path = require('path')
 var OfflinePlugin = require('offline-plugin')
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
@@ -11,7 +12,12 @@ function generateConfig() {
   function styleLoader() {
     const ret = []
     ret.push({
-      loader: 'style-loader'
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        // you can specify a publicPath here
+        // by default it use publicPath in webpackOptions.output
+        publicPath: '../'
+      }
     })
     ret.push({
       loader: 'css-loader'
@@ -51,6 +57,20 @@ function generateConfig() {
       filename: config.packageName + '.js',
       library: config.packageName,
       libraryTarget: 'umd'
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        minSize: 200000,
+        maxSize: 244000,
+        cacheGroups: {
+          default: {
+            minChunks: 2,
+            priority: 1,
+            reuseExistingChunk: true
+          }
+        }
+      }
     },
     devServer: {
       contentBase: path.join(__dirname, "./public"),
@@ -97,6 +117,12 @@ function generateConfig() {
     plugins: [
       new webpack.DefinePlugin({
         'process.env.config': JSON.stringify(config)
+      }),
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "[name].css",
+        chunkFilename: "[id].css"
       }),
       new HtmlWebpackPlugin({
         title: config.title,
