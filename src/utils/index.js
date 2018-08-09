aa12c6aacd43db5336d9bf8924f64343ca7d0d0c
nainemom <nainemom@gmail.com>
2018-08-10 01:59:10 +0430

    add faNumbers method

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,17 @@ const config = process.env.config;
 export function getConfig() {
   return config
 }
+
+export function faNumbers(value) {
+  if (typeof value === "number") {
+    var value = value.toString();
+  }
+
+  return value.split('').reduce(function (result, char) {
+    var charCode = char.charCodeAt(0)
+    if (charCode >= 48 && charCode <= 57) { // when charachter is 0 to 9
+      return result + String.fromCharCode(charCode + 1728)
+    }
+    return result + char;
+  }, "");
+}
