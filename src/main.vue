0bdba9584d4d87e858fe6e6d278f635f3837dfbb
nainemom <nainemom@gmail.com>
2018-08-12 02:50:12 +0430

    check login on client app starts

--- src/main.vue
+++ src/main.vue
@@ -1,3 +1,13 @@
 <template>
   <router-view></router-view>
 </template>
+
+<script>
+import { login } from "./utils/github.js";
+
+export default {
+  created() {
+    console.log(this.$cookie.get("github_token"));
+  }
+};
+</script>
