aebd7716f70386ab05e47a33677f96cfa6e4d7ed
nainemom <nainemom@gmail.com>
2018-08-12 18:44:50 +0430

    login if needed in starup

--- src/main.vue
+++ src/main.vue
@@ -3,16 +3,9 @@
 </template>
 
 <script>
-import { deserialize } from "./utils/index.js";
-import { login } from "./utils/github.js";
-
 export default {
   async created() {
-    const githubData = this.$cookie.get("github_token");
-    if (githubData) {
-      const parsed = deserialize(githubData);
-      await login(parsed);
-    }
+    await this.$store.dispatch("setAccessTokenIfNeeded");
   }
 };
 </script>
